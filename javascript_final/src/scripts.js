8dbfd8f0e62e9f0bc62034fccdfb50348b00137b
Natasha Tolia <tolin478@newschool.edu>
2020-05-12 16:46:18 -0700

    Add files via upload

--- javascript_final/src/scripts.js
+++ javascript_final/src/scripts.js
@@ -13,25 +13,6 @@ fetch(url)
     });
   });
 
-
-  // var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-  //     targetUrl = url="https://trefle.io/api/auth/claim?token=K2plQjBFaUhNcGNMUHRSRzFGaDJSUT09";
-  // fetch(proxyUrl + targetUrl)
-  //   .then(
-  //     function(response){
-  //       if(response.status != 200){
-  //         console.log('looks like there was a problem');
-  //         return;
-  //       }
-  //
-  //     response.json().then(function(data){
-  //       console.log(data);
-  //     });
-  //   });
-
-function compareStrings(string1,string2){
-}
-
-function submitplant(){
+function compareStrings(){
 
 }
